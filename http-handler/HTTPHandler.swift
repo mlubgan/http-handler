547f75db03bc01fa22d09ab7cdb6f14182fdef76
Karol Wawrzyniak <karol.wawrzyniak@fivedottwelve.com>
2018-10-18 22:05:44 +0200

    version 0.01

---
+++ http-handler/HTTPHandler.swift
@@ -0,0 +1,249 @@
+//
+//  HttpHandler.swift
+//  http-handler
+//
+//  Created by Karol Wawrzyniak on 18/10/2018.
+//  Copyright © 2018 Karol Wawrzyniak. All rights reserved.
+//
+
+import Foundation
+import Unbox
+
+public typealias CompletionBlock<T> = (T?, Error?) -> Void
+
+public protocol HttpHandlerRequest {
+
+    func endPoint() -> String
+
+    func method() -> String
+
+    func parameters() -> Dictionary<String, Any>?
+
+    func headers() -> Dictionary<String, String>
+
+}
+
+public enum HttpHandlerError: Error {
+    case WrongStatusCode(message: String?)
+    case ServerResponseNotParseable(message: String?)
+    case NotHttpResponse(message: String?)
+    case NoDataFromServer
+    case NotExpetedDatastructureFromServer(message: String?)
+    case ServerResponseIsNotUnboxableDictionary(message: String?)
+    case ServerReportedUnsuccessfulOperation
+    case ServerResponseReturnedError(errors: String?)
+}
+
+extension HttpHandlerError: LocalizedError {
+
+    private func concatMessage(error: String, message: String?) -> String {
+
+        var result = error
+
+        if let m = message {
+            result.append(" : ")
+            result.append(m)
+        }
+
+        return result
+    }
+
+    public var errorDescription: String? {
+        switch self {
+
+        case .NotHttpResponse(message: let message):
+
+            return concatMessage(error: NSLocalizedString("Not http response", comment: ""), message: message)
+
+        case .WrongStatusCode(message: let message):
+
+            return concatMessage(error: NSLocalizedString("Wrong http status code", comment: ""), message: message)
+
+        case .ServerResponseNotParseable(message: let message):
+
+            return concatMessage(error: NSLocalizedString("Bad server response - not parsable", comment: ""), message: message)
+
+        case .NoDataFromServer:
+            return NSLocalizedString("No data from server", comment: "")
+
+        case .NotExpetedDatastructureFromServer(message: let message):
+
+            return concatMessage(error: NSLocalizedString("Not expected data structure from server", comment: ""), message: message)
+
+        case .ServerResponseIsNotUnboxableDictionary(message: let message):
+
+            return concatMessage(error: NSLocalizedString("Server response is not unboxable", comment: ""), message: message)
+
+        case .ServerReportedUnsuccessfulOperation:
+            return NSLocalizedString("Server reported unsuccessful operation", comment: "")
+
+        case .ServerResponseReturnedError(errors: let errors):
+
+            return concatMessage(error: NSLocalizedString("Server response is not unboxable", comment: ""), message: errors)
+        }
+    }
+
+}
+
+public protocol IHTTPHandler: class {
+
+    func make<T>(request: HttpHandlerRequest, completion: @escaping (T?, Error?) -> Void)
+
+    func make<T: Unboxable>(request: HttpHandlerRequest, completion: @escaping (T?, Error?) -> Void)
+
+    func make<T>(request: HttpHandlerRequest, completion: @escaping (T?, [AnyHashable: Any], Error?) -> Void)
+
+}
+
+class Response<T: Unboxable>: Unboxable {
+
+    var result: T
+
+    required init(unboxer: Unboxer) throws {
+        self.result = try unboxer.unbox(key: "result")
+    }
+}
+
+
+public class HTTPHandler: IHTTPHandler {
+
+    let urlSession: URLSession
+    let baseURL: String
+
+
+    public init(baseURL: String) {
+        self.baseURL = baseURL
+        self.urlSession = URLSession(configuration: .default)
+    }
+
+    fileprivate func handleResponse<T>(_ error: Error?, _ response: HTTPURLResponse, _ data: Data?, completion: @escaping (T?, [AnyHashable: Any], Error?) -> Void) {
+        DispatchQueue.main.async {
+
+            if error != nil {
+                completion(nil, response.allHeaderFields, error)
+                return
+            }
+
+            if let dataToParse = data {
+
+                guard response.statusCode == 200 else {
+                    completion(nil, response.allHeaderFields, HttpHandlerError.WrongStatusCode(message: response.debugDescription))
+                    return
+                }
+
+                guard let parsedData = try? JSONSerialization.jsonObject(with: dataToParse) else {
+                    let jsonString = String(data: dataToParse, encoding: String.Encoding.utf8)
+                    completion(nil, response.allHeaderFields, HttpHandlerError.ServerResponseNotParseable(message: jsonString))
+                    return
+                }
+
+                if let parsedData = parsedData as? T {
+                    completion(parsedData, response.allHeaderFields, error)
+                } else {
+                    let jsonString = String(data: dataToParse, encoding: String.Encoding.utf8)
+                    completion(nil, response.allHeaderFields, HttpHandlerError.ServerResponseNotParseable(message: jsonString))
+                }
+
+            } else {
+                completion(nil, response.allHeaderFields, HttpHandlerError.NoDataFromServer)
+            }
+
+        }
+    }
+
+    private static var numberOfCallsToSetVisible: Int = 0
+
+    static func setVisibleActivitiIndicator(visible: Bool) {
+        if visible {
+            HTTPHandler.numberOfCallsToSetVisible = HTTPHandler.numberOfCallsToSetVisible + 1
+        } else {
+            HTTPHandler.numberOfCallsToSetVisible = HTTPHandler.numberOfCallsToSetVisible - 1
+        }
+
+        DispatchQueue.main.async {
+            UIApplication.shared.isNetworkActivityIndicatorVisible = HTTPHandler.numberOfCallsToSetVisible > 0
+        }
+    }
+
+    public func make<T: Unboxable>(request: HttpHandlerRequest, completion: @escaping (T?, Error?) -> Void) {
+
+        self.run(request: request) { (result: UnboxableDictionary?, headers: [AnyHashable: Any], error: Error?) in
+
+            if let error = error {
+                completion(nil, error)
+                return
+            }
+            guard let result = result else {
+                completion(nil, HttpHandlerError.NoDataFromServer)
+                return
+            }
+            do {
+                let unboxed: T = try unbox(dictionary: result)
+                completion(unboxed, nil)
+            } catch let error {
+                completion(nil, error)
+            }
+
+        }
+    }
+
+    func run<T>(request: HttpHandlerRequest, completion: @escaping (T?, [AnyHashable: Any], Error?) -> Void) {
+
+        guard let url = URL(string: self.baseURL + request.endPoint()) else { return }
+        var urlRequest = URLRequest(url: url, cachePolicy: .reloadIgnoringLocalAndRemoteCacheData, timeoutInterval: 60)
+        urlRequest.httpMethod = request.method()
+
+        let headers = request.headers()
+
+        for (key, value) in headers {
+            urlRequest.addValue(value, forHTTPHeaderField: key)
+        }
+
+        if let params = request.parameters(), request.method() != "GET" {
+            do {
+                let paramsData = try JSONSerialization.data(withJSONObject: params, options: JSONSerialization.WritingOptions(rawValue: 0))
+                urlRequest.httpBody = paramsData
+            } catch {
+                print("HttpHandler error \(error.localizedDescription)")
+            }
+        }
+
+        HTTPHandler.setVisibleActivitiIndicator(visible: true)
+
+        let task = self.urlSession.dataTask(with: urlRequest) { [weak self] (data, pResponse, error) in
+            HTTPHandler.setVisibleActivitiIndicator(visible: false)
+
+            guard let `self` = self else {
+                return
+            }
+
+            guard let response = pResponse as? HTTPURLResponse else {
+
+                if let error = error {
+                    completion(nil, [:], error)
+                } else {
+                    let responseInfo = pResponse.debugDescription
+                    completion(nil, [:], HttpHandlerError.NotHttpResponse(message: responseInfo))
+                }
+
+                return
+            }
+
+            self.handleResponse(error, response, data, completion: completion)
+
+        }
+
+        task.resume()
+    }
+
+    public func make<T>(request: HttpHandlerRequest, completion: @escaping (T?, [AnyHashable: Any], Error?) -> Void) {
+        self.run(request: request, completion: completion)
+    }
+
+    public func make<T>(request: HttpHandlerRequest, completion: @escaping (T?, Error?) -> Void) {
+        self.run(request: request) { (result, headers, error) in
+            completion(result, error)
+        }
+    }
+
+}
---
+++ http-handler/http_handler.h
@@ -0,0 +1,19 @@
+//
+//  http_handler.h
+//  http-handler
+//
+//  Created by Karol Wawrzyniak on 18/10/2018.
+//  Copyright © 2018 KAWA. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for http_handler.
+FOUNDATION_EXPORT double http_handlerVersionNumber;
+
+//! Project version string for http_handler.
+FOUNDATION_EXPORT const unsigned char http_handlerVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <http_handler/PublicHeader.h>
+
+
